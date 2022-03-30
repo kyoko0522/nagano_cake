8679515a892a02e84e7fedb3f09c1448645c825d
kyoko0522 <kyo08023971867@gmail.com>
2022-03-30 15:11:40 +0000

    [Update]2

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -17,20 +17,51 @@ end
 
 
 def create
-   @order = Order.new(order_params)
+   @cart_items = current_customer.cart_items.all
+   @order = current_customer.orders.new(order_params)
    @order.customer_id = current_customer.id
-   @order.save
+   if @order.save
+
+   cart_items.each do |cart|
+       order_detail = OrderDetail.new
+       order_detail.item_id = cart_item.id
+       order_detail.order_id = @order.id
+       order_detail.amount = cart_item.amount
+       order_detail.save
+   end
    redirect_to orders_complete_path
+   cart_item.destroy_all
+   else
+    @order = Order.new(order_params)
+    render :new
+   end
 end
 
 def confirm
-    @current_customer_cart_items = current_customer.cart_items
-    @order = Order.new(order_params)
-    @order.postal_code = current_customer.postal_code
-    @order.address = current_customer.address
 
+    @order = Order.new(order_params)
     # @order.name = current_customer.first_name + current_customer.last_name
     @total = 0
+	@current_customer_cart_items = current_customer.cart_items
+	@order.payment_method = params[:order][:payment_method]
+
+	if params[:order][:address_number] == "0"
+	   @order.name = current_customer.name
+	   @order.postal_code = current_customer.postal_code
+	   @order.address = current_customer.address
+	elsif params[:order][:addres_number] == "1"
+		@sta = params[:order][:order_address].to_i
+		binding.pry
+		@order_address = Address.find(@sta)
+		@order.postal_code = @order_address.postal_code
+		@order.address = @order_address.address
+		@order.name = @order_address.dear_name
+
+	elsif params[:order][:address_number] == "2"
+		@order.postal_code = params[:order][:postal_code]
+		@order.address = params[:order][:order_address]
+	end
+
 end
 
 def complete
