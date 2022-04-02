b64d67dc9aa794d67b49491cf1c833b42f45b0a1
kyoko0522 <kyo08023971867@gmail.com>
2022-04-02 01:08:18 +0000

    [Update]2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_26_093708) do
+ActiveRecord::Schema.define(version: 2022_04_02_000142) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "name"
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2022_03_26_093708) do
     t.string "postal_code"
     t.string "address"
     t.string "telephone_number"
-    t.boolean "is_deleted"
+    t.boolean "is_deleted", default: false, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
