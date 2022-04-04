1d604e90811fd16f883650f1cf1d5f06a03e8d75
kyoko0522 <kyo08023971867@gmail.com>
2022-04-04 05:26:08 +0000

    [Update]2

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -3,6 +3,9 @@
 class Admin::SessionsController < Devise::SessionsController
  layout 'admin'
 
+   def after_sign_out_path_for(resource)
+    new_admin_session_path
+   end
 
   def after_sign_in_path_for(resource)
     admin_root_path
